3dc483b1211add62ad10df23ce83ce85c1e2b6ae
Rohit <rohitsahu728@gmail.com>
2020-03-13 22:07:21 +0530

    initial commit

---
+++ blog/urls.py
@@ -0,0 +1,12 @@
+from django.contrib import admin
+from django.urls import path
+from .views import home, about, contact, login, register, privacy
+
+urlpatterns = [
+    path('', home, name = "home"),
+    path('about/', about, name = "about"),
+    path('contact/', contact, name = "contact"),
+    path('login/', login, name = "login"),
+    path('register/', register, name = "register"),
+    path('privacy-policy/', privacy, name = "privacy"),
+]
---
+++ blog/views.py
@@ -0,0 +1,21 @@
+from django.shortcuts import render
+from django.shortcuts import HttpResponse
+# Create your views here.
+
+def home(request):
+    return render(request, 'index.html')
+
+def about(request):
+    return render(request, 'about.html')
+
+def contact(request):
+    return render(request, 'contact.html')
+
+def login(request):
+    return render(request, 'login.html')
+
+def register(request):
+    return render(request, 'register.html')
+
+def privacy(request):
+    return render(request, 'privacy-policy.html')
